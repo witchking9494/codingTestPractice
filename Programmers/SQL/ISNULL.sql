7f8b4b126371a677338d663e199ba0800ef7cd94
witchking9494 <oilsu2@naver.com>
2025-02-04 08:17:10 +0900

    isnull

--- Programmers/SQL/ISNULL.sql
+++ Programmers/SQL/ISNULL.sql
@@ -36,3 +36,24 @@ SELECT COUNT(*) AS USERS FROM USER_INFO WHERE AGE IS NULL;
 -- 잡은 물고기의 평균 길이 구하기 / MySQL
 -- https://school.programmers.co.kr/learn/courses/30/lessons/293259
 SELECT ROUND(AVG(IFNULL(LENGTH, 10)), 2) AS AVERAGE_LENGTH FROM FISH_INFO
+
+
+
+-- ROOT 아이템 구하기 / MySQL
+-- https://school.programmers.co.kr/learn/courses/30/lessons/273710
+SELECT A.ITEM_ID, A.ITEM_NAME FROM ITEM_INFO A 
+JOIN ITEM_TREE B ON A.ITEM_ID = B.ITEM_ID
+    WHERE B.PARENT_ITEM_ID IS NULL
+
+
+
+-- 업그레이드 할 수 없는 아이템 구하기 / MySQL
+-- https://school.programmers.co.kr/learn/courses/30/lessons/273712
+SELECT A.ITEM_ID, A.ITEM_NAME, A.RARITY 
+        FROM ITEM_INFO A 
+        JOIN ITEM_TREE B ON A.ITEM_ID = B.ITEM_ID
+    WHERE A.ITEM_ID NOT IN 
+        (SELECT PARENT_ITEM_ID 
+                FROM ITEM_TREE C 
+            WHERE PARENT_ITEM_ID IS NOT NULL)
+    ORDER BY ITEM_ID DESC
